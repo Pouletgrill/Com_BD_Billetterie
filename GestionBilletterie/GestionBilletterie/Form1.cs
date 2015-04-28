a30801e832ed7c870acb43b0158e1dfd80a25ad4
PouletGrill <xavierb.2006@hotmail.com>
2015-04-28 09:47:49 -0400

    qwerty

--- GestionBilletterie/GestionBilletterie/Form1.cs
+++ GestionBilletterie/GestionBilletterie/Form1.cs
@@ -7,19 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
 
 namespace GestionBilletterie
 {
    public partial class Form1 : Form
    {
-      public Form1()
-      {
+      DataSet dataSet = new DataSet();
+      OracleConnection conn;
+      public Form1(OracleConnection connection)
+      {         
          InitializeComponent();
+         conn = connection;
       }
 
       private void Form1_Load(object sender, EventArgs e)
       {
 
+      }
+      public void RefreshDGVSpectacle()
+      {
+         try
+         {
+            // //déclaration de OracleCommand pour appeler la fonction avec la
+            //connection conn.
+            OracleCommand Oracmd = new OracleCommand("PKG_GESTION",
+            conn);
+            Oracmd.CommandText = "PKG_GESTION.AFFICHERQUESTION";
+            Oracmd.CommandType = CommandType.StoredProcedure;
+            // pour une fonction, le paramètre de retour doit être déclaré en
+            //premier.
+            OracleParameter OrapameResultat = new
+            OracleParameter("CURSORGESTION", OracleDbType.RefCursor);
+            OrapameResultat.Direction = ParameterDirection.ReturnValue;
+            Oracmd.Parameters.Add(OrapameResultat);
+
+            // Pour remplir le DataSet, on déclare un OracleDataAdapter pour lequel
+            // on passe notre OracleCommand qui contient TOUS les paramètres.
+
+            OracleDataAdapter orAdater = new OracleDataAdapter(Oracmd);
+            if (dataSet.Tables.Contains("Question"))
+            {
+               dataSet.Tables["Question"].Clear();
+            }
+            orAdater.Fill(dataSet, "Question");
+            Oracmd.Dispose();
+            BindingSource bindingSource;
+            bindingSource = new BindingSource(dataSet, "Question");
+             DGV_Spectacle.DataSource = bindingSource;
+             DGV_Spectacle.Columns[2].Visible = false;
+         }
+         catch (Exception se)
+         {
+            MessageBox.Show(se.Message.ToString());
+         }
       }
    }
 }
--- GestionBilletterie/GestionBilletterie/Menu.cs
+++ GestionBilletterie/GestionBilletterie/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,56 @@ namespace GestionBilletterie
 {
    public partial class Menu : Form
    {
+      public OracleConnection connection;
+
+      private static String orclUser = "brosseau";
+      private static string orclPass = "ORACLE2";
+      public String connectionString = "Data Source=(DESCRIPTION="
+                             + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)"
+                             + "(HOST=205.237.244.251)(PORT=1521)))"
+                             + "(CONNECT_DATA=(SERVICE_NAME=ORCL.clg.qc.ca)));"
+                             + "User Id=" + orclUser + ";Password="+orclPass;
       public Menu()
       {
          InitializeComponent();
+      }
+
+      private void Menu_Load(object sender, EventArgs e)
+      {
+         Connecter();
+      }
+
+      private void Connecter()
+      {
+         connection = new OracleConnection(connectionString);
+         connection.ConnectionString = connectionString;
+
+         try
+         {
+            connection.Open();
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show("Erreur de connexion");
+         }
+      }
+
+      private void Menu_FormClosing(object sender, FormClosingEventArgs e)
+      {
+         try
+         {
+            connection.Close();
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show("Erreur de deconnexion");
+         }
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         Form1 dlg = new Form1(connection);
+         dlg.ShowDialog();
       }
    }
 }
