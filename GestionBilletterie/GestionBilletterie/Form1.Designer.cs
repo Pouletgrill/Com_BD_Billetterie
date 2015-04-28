3a9a2869c988ac1e4df0b1dfc0421a97a75d0a93
PouletGrill <xavierb.2006@hotmail.com>
2015-04-28 09:44:24 -0400

    connexion fait

--- GestionBilletterie/GestionBilletterie/Form1.Designer.cs
+++ GestionBilletterie/GestionBilletterie/Form1.Designer.cs
@@ -29,19 +29,19 @@
       private void InitializeComponent()
       {
          this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-         this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-         this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-         this.editionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.DGV_Spectacle = new System.Windows.Forms.DataGridView();
          this.GB_Spectacle = new System.Windows.Forms.GroupBox();
-         this.BTN_Ajouter_Spectacle = new System.Windows.Forms.Button();
-         this.BTN_Modifier_Spectacle = new System.Windows.Forms.Button();
          this.BTN_Supprimer_Spectacle = new System.Windows.Forms.Button();
+         this.BTN_Modifier_Spectacle = new System.Windows.Forms.Button();
+         this.BTN_Ajouter_Spectacle = new System.Windows.Forms.Button();
+         this.DGV_Representation = new System.Windows.Forms.DataGridView();
          this.GB_Representation = new System.Windows.Forms.GroupBox();
          this.button1 = new System.Windows.Forms.Button();
          this.button2 = new System.Windows.Forms.Button();
          this.button3 = new System.Windows.Forms.Button();
-         this.dataGridView1 = new System.Windows.Forms.DataGridView();
-         this.dataGridView2 = new System.Windows.Forms.DataGridView();
+         this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+         this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+         this.editionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
          this.statusStrip1 = new System.Windows.Forms.StatusStrip();
          this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
          this.LB_ConnexionStatus = new System.Windows.Forms.ToolStripStatusLabel();
@@ -49,11 +49,11 @@
          this.splitContainer1.Panel1.SuspendLayout();
          this.splitContainer1.Panel2.SuspendLayout();
          this.splitContainer1.SuspendLayout();
-         this.menuStrip1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.DGV_Spectacle)).BeginInit();
          this.GB_Spectacle.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.DGV_Representation)).BeginInit();
          this.GB_Representation.SuspendLayout();
-         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-         ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+         this.menuStrip1.SuspendLayout();
          this.statusStrip1.SuspendLayout();
          this.SuspendLayout();
          // 
@@ -67,39 +67,29 @@
          // 
          // splitContainer1.Panel1
          // 
-         this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+         this.splitContainer1.Panel1.Controls.Add(this.DGV_Spectacle);
          this.splitContainer1.Panel1.Controls.Add(this.GB_Spectacle);
+         this.splitContainer1.Panel1MinSize = 254;
          // 
          // splitContainer1.Panel2
          // 
-         this.splitContainer1.Panel2.Controls.Add(this.dataGridView2);
+         this.splitContainer1.Panel2.Controls.Add(this.DGV_Representation);
          this.splitContainer1.Panel2.Controls.Add(this.GB_Representation);
+         this.splitContainer1.Panel2MinSize = 258;
          this.splitContainer1.Size = new System.Drawing.Size(518, 373);
-         this.splitContainer1.SplitterDistance = 257;
+         this.splitContainer1.SplitterDistance = 254;
          this.splitContainer1.TabIndex = 0;
          // 
-         // menuStrip1
+         // DGV_Spectacle
          // 
-         this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fichierToolStripMenuItem,
-            this.editionToolStripMenuItem});
-         this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-         this.menuStrip1.Name = "menuStrip1";
-         this.menuStrip1.Size = new System.Drawing.Size(521, 24);
-         this.menuStrip1.TabIndex = 1;
-         this.menuStrip1.Text = "menuStrip1";
-         // 
-         // fichierToolStripMenuItem
-         // 
-         this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
-         this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
-         this.fichierToolStripMenuItem.Text = "Fichier";
-         // 
-         // editionToolStripMenuItem
-         // 
-         this.editionToolStripMenuItem.Name = "editionToolStripMenuItem";
-         this.editionToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
-         this.editionToolStripMenuItem.Text = "Edition";
+         this.DGV_Spectacle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.DGV_Spectacle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         this.DGV_Spectacle.Location = new System.Drawing.Point(3, 61);
+         this.DGV_Spectacle.Name = "DGV_Spectacle";
+         this.DGV_Spectacle.Size = new System.Drawing.Size(250, 309);
+         this.DGV_Spectacle.TabIndex = 1;
          // 
          // GB_Spectacle
          // 
@@ -113,14 +103,14 @@
          this.GB_Spectacle.TabStop = false;
          this.GB_Spectacle.Text = "Spectacle";
          // 
-         // BTN_Ajouter_Spectacle
+         // BTN_Supprimer_Spectacle
          // 
-         this.BTN_Ajouter_Spectacle.Location = new System.Drawing.Point(7, 20);
-         this.BTN_Ajouter_Spectacle.Name = "BTN_Ajouter_Spectacle";
-         this.BTN_Ajouter_Spectacle.Size = new System.Drawing.Size(75, 23);
-         this.BTN_Ajouter_Spectacle.TabIndex = 0;
-         this.BTN_Ajouter_Spectacle.Text = "Ajouter";
-         this.BTN_Ajouter_Spectacle.UseVisualStyleBackColor = true;
+         this.BTN_Supprimer_Spectacle.Location = new System.Drawing.Point(169, 20);
+         this.BTN_Supprimer_Spectacle.Name = "BTN_Supprimer_Spectacle";
+         this.BTN_Supprimer_Spectacle.Size = new System.Drawing.Size(75, 23);
+         this.BTN_Supprimer_Spectacle.TabIndex = 2;
+         this.BTN_Supprimer_Spectacle.Text = "Supprimer";
+         this.BTN_Supprimer_Spectacle.UseVisualStyleBackColor = true;
          // 
          // BTN_Modifier_Spectacle
          // 
@@ -131,14 +121,25 @@
          this.BTN_Modifier_Spectacle.Text = "Modifier";
          this.BTN_Modifier_Spectacle.UseVisualStyleBackColor = true;
          // 
-         // BTN_Supprimer_Spectacle
+         // BTN_Ajouter_Spectacle
          // 
-         this.BTN_Supprimer_Spectacle.Location = new System.Drawing.Point(169, 20);
-         this.BTN_Supprimer_Spectacle.Name = "BTN_Supprimer_Spectacle";
-         this.BTN_Supprimer_Spectacle.Size = new System.Drawing.Size(75, 23);
-         this.BTN_Supprimer_Spectacle.TabIndex = 2;
-         this.BTN_Supprimer_Spectacle.Text = "Supprimer";
-         this.BTN_Supprimer_Spectacle.UseVisualStyleBackColor = true;
+         this.BTN_Ajouter_Spectacle.Location = new System.Drawing.Point(7, 20);
+         this.BTN_Ajouter_Spectacle.Name = "BTN_Ajouter_Spectacle";
+         this.BTN_Ajouter_Spectacle.Size = new System.Drawing.Size(75, 23);
+         this.BTN_Ajouter_Spectacle.TabIndex = 0;
+         this.BTN_Ajouter_Spectacle.Text = "Ajouter";
+         this.BTN_Ajouter_Spectacle.UseVisualStyleBackColor = true;
+         // 
+         // DGV_Representation
+         // 
+         this.DGV_Representation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.DGV_Representation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         this.DGV_Representation.Location = new System.Drawing.Point(3, 61);
+         this.DGV_Representation.Name = "DGV_Representation";
+         this.DGV_Representation.Size = new System.Drawing.Size(250, 309);
+         this.DGV_Representation.TabIndex = 2;
          // 
          // GB_Representation
          // 
@@ -179,27 +180,28 @@
          this.button3.Text = "Ajouter";
          this.button3.UseVisualStyleBackColor = true;
          // 
-         // dataGridView1
+         // menuStrip1
          // 
-         this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-         this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-         this.dataGridView1.Location = new System.Drawing.Point(3, 61);
-         this.dataGridView1.Name = "dataGridView1";
-         this.dataGridView1.Size = new System.Drawing.Size(250, 309);
-         this.dataGridView1.TabIndex = 1;
+         this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.editionToolStripMenuItem});
+         this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+         this.menuStrip1.Name = "menuStrip1";
+         this.menuStrip1.Size = new System.Drawing.Size(521, 24);
+         this.menuStrip1.TabIndex = 1;
+         this.menuStrip1.Text = "menuStrip1";
          // 
-         // dataGridView2
+         // fichierToolStripMenuItem
          // 
-         this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-         this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-         this.dataGridView2.Location = new System.Drawing.Point(3, 61);
-         this.dataGridView2.Name = "dataGridView2";
-         this.dataGridView2.Size = new System.Drawing.Size(250, 309);
-         this.dataGridView2.TabIndex = 2;
+         this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+         this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+         this.fichierToolStripMenuItem.Text = "Fichier";
+         // 
+         // editionToolStripMenuItem
+         // 
+         this.editionToolStripMenuItem.Name = "editionToolStripMenuItem";
+         this.editionToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+         this.editionToolStripMenuItem.Text = "Edition";
          // 
          // statusStrip1
          // 
@@ -221,8 +223,8 @@
          // LB_ConnexionStatus
          // 
          this.LB_ConnexionStatus.Name = "LB_ConnexionStatus";
-         this.LB_ConnexionStatus.Size = new System.Drawing.Size(43, 17);
-         this.LB_ConnexionStatus.Text = "ERROR";
+         this.LB_ConnexionStatus.Size = new System.Drawing.Size(58, 17);
+         this.LB_ConnexionStatus.Text = "Connecté";
          // 
          // Form1
          // 
@@ -235,17 +237,18 @@
          this.MainMenuStrip = this.menuStrip1;
          this.MinimumSize = new System.Drawing.Size(537, 149);
          this.Name = "Form1";
-         this.Text = "Form1";
+         this.Text = "Gestion spectacle";
+         this.Load += new System.EventHandler(this.Form1_Load);
          this.splitContainer1.Panel1.ResumeLayout(false);
          this.splitContainer1.Panel2.ResumeLayout(false);
          ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
          this.splitContainer1.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.DGV_Spectacle)).EndInit();
+         this.GB_Spectacle.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.DGV_Representation)).EndInit();
+         this.GB_Representation.ResumeLayout(false);
          this.menuStrip1.ResumeLayout(false);
          this.menuStrip1.PerformLayout();
-         this.GB_Spectacle.ResumeLayout(false);
-         this.GB_Representation.ResumeLayout(false);
-         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-         ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
          this.statusStrip1.ResumeLayout(false);
          this.statusStrip1.PerformLayout();
          this.ResumeLayout(false);
@@ -267,8 +270,8 @@
       private System.Windows.Forms.MenuStrip menuStrip1;
       private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
       private System.Windows.Forms.ToolStripMenuItem editionToolStripMenuItem;
-      private System.Windows.Forms.DataGridView dataGridView1;
-      private System.Windows.Forms.DataGridView dataGridView2;
+      private System.Windows.Forms.DataGridView DGV_Spectacle;
+      private System.Windows.Forms.DataGridView DGV_Representation;
       private System.Windows.Forms.StatusStrip statusStrip1;
       private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
       private System.Windows.Forms.ToolStripStatusLabel LB_ConnexionStatus;
