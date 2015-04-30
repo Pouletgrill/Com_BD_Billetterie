ff3d2cc28ff8ddc3e29e06c70f7ff84e69a2469b
PouletGrill <xavierb.2006@hotmail.com>
2015-04-30 09:49:21 -0400

    Traitement d'image par Imgur :swimmer:

--- GestionBilletterie/GestionBilletterie/AjouterModifierSpectacle.Designer.cs
+++ GestionBilletterie/GestionBilletterie/AjouterModifierSpectacle.Designer.cs
@@ -28,137 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.CB_Categorie = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.TB_Titre = new System.Windows.Forms.TextBox();
-            this.TB_Artiste = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.BTN_Parcourir = new System.Windows.Forms.Button();
-            this.BTN_OK = new System.Windows.Forms.Button();
-            this.BTN_Cancel = new System.Windows.Forms.Button();
-            this.PN_Poster = new System.Windows.Forms.Panel();
-            this.SuspendLayout();
-            // 
-            // CB_Categorie
-            // 
-            this.CB_Categorie.FormattingEnabled = true;
-            this.CB_Categorie.Location = new System.Drawing.Point(76, 6);
-            this.CB_Categorie.Name = "CB_Categorie";
-            this.CB_Categorie.Size = new System.Drawing.Size(121, 21);
-            this.CB_Categorie.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Catégorie :";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Titre :";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 62);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Artiste :";
-            // 
-            // TB_Titre
-            // 
-            this.TB_Titre.Location = new System.Drawing.Point(76, 33);
-            this.TB_Titre.Name = "TB_Titre";
-            this.TB_Titre.Size = new System.Drawing.Size(121, 20);
-            this.TB_Titre.TabIndex = 4;
-            // 
-            // TB_Artiste
-            // 
-            this.TB_Artiste.Location = new System.Drawing.Point(76, 59);
-            this.TB_Artiste.Name = "TB_Artiste";
-            this.TB_Artiste.Size = new System.Drawing.Size(121, 20);
-            this.TB_Artiste.TabIndex = 5;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 90);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Affiche :";
-            // 
-            // BTN_Parcourir
-            // 
-            this.BTN_Parcourir.Location = new System.Drawing.Point(76, 85);
-            this.BTN_Parcourir.Name = "BTN_Parcourir";
-            this.BTN_Parcourir.Size = new System.Drawing.Size(121, 23);
-            this.BTN_Parcourir.TabIndex = 7;
-            this.BTN_Parcourir.Text = "Parcourir";
-            this.BTN_Parcourir.UseVisualStyleBackColor = true;
-            // 
-            // BTN_OK
-            // 
-            this.BTN_OK.Location = new System.Drawing.Point(117, 119);
-            this.BTN_OK.Name = "BTN_OK";
-            this.BTN_OK.Size = new System.Drawing.Size(80, 23);
-            this.BTN_OK.TabIndex = 8;
-            this.BTN_OK.Text = "OK";
-            this.BTN_OK.UseVisualStyleBackColor = true;
-            this.BTN_OK.Click += new System.EventHandler(this.BTN_OK_Click);
-            // 
-            // BTN_Cancel
-            // 
-            this.BTN_Cancel.Location = new System.Drawing.Point(31, 119);
-            this.BTN_Cancel.Name = "BTN_Cancel";
-            this.BTN_Cancel.Size = new System.Drawing.Size(80, 23);
-            this.BTN_Cancel.TabIndex = 9;
-            this.BTN_Cancel.Text = "Annuler";
-            this.BTN_Cancel.UseVisualStyleBackColor = true;
-            this.BTN_Cancel.Click += new System.EventHandler(this.BTN_Cancel_Click);
-            // 
-            // PN_Poster
-            // 
-            this.PN_Poster.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PN_Poster.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.PN_Poster.Location = new System.Drawing.Point(203, 6);
-            this.PN_Poster.Name = "PN_Poster";
-            this.PN_Poster.Size = new System.Drawing.Size(99, 136);
-            this.PN_Poster.TabIndex = 10;
-            // 
-            // AjouterModifierSpectacle
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(309, 154);
-            this.Controls.Add(this.PN_Poster);
-            this.Controls.Add(this.BTN_Cancel);
-            this.Controls.Add(this.BTN_OK);
-            this.Controls.Add(this.BTN_Parcourir);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.TB_Artiste);
-            this.Controls.Add(this.TB_Titre);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.CB_Categorie);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "AjouterModifierSpectacle";
-            this.Text = "AjouterModifierSpectacle";
-            this.Load += new System.EventHandler(this.AjouterModifierSpectacle_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+         this.CB_Categorie = new System.Windows.Forms.ComboBox();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.TB_Titre = new System.Windows.Forms.TextBox();
+         this.TB_Artiste = new System.Windows.Forms.TextBox();
+         this.label4 = new System.Windows.Forms.Label();
+         this.BTN_Parcourir = new System.Windows.Forms.Button();
+         this.BTN_OK = new System.Windows.Forms.Button();
+         this.BTN_Cancel = new System.Windows.Forms.Button();
+         this.PB_Poster = new System.Windows.Forms.PictureBox();
+         ((System.ComponentModel.ISupportInitialize)(this.PB_Poster)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // CB_Categorie
+         // 
+         this.CB_Categorie.FormattingEnabled = true;
+         this.CB_Categorie.Location = new System.Drawing.Point(76, 6);
+         this.CB_Categorie.Name = "CB_Categorie";
+         this.CB_Categorie.Size = new System.Drawing.Size(121, 21);
+         this.CB_Categorie.TabIndex = 0;
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(12, 9);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(58, 13);
+         this.label1.TabIndex = 1;
+         this.label1.Text = "Catégorie :";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(36, 36);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(34, 13);
+         this.label2.TabIndex = 2;
+         this.label2.Text = "Titre :";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(28, 62);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(42, 13);
+         this.label3.TabIndex = 3;
+         this.label3.Text = "Artiste :";
+         // 
+         // TB_Titre
+         // 
+         this.TB_Titre.Location = new System.Drawing.Point(76, 33);
+         this.TB_Titre.Name = "TB_Titre";
+         this.TB_Titre.Size = new System.Drawing.Size(121, 20);
+         this.TB_Titre.TabIndex = 4;
+         // 
+         // TB_Artiste
+         // 
+         this.TB_Artiste.Location = new System.Drawing.Point(76, 59);
+         this.TB_Artiste.Name = "TB_Artiste";
+         this.TB_Artiste.Size = new System.Drawing.Size(121, 20);
+         this.TB_Artiste.TabIndex = 5;
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(24, 90);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(46, 13);
+         this.label4.TabIndex = 6;
+         this.label4.Text = "Affiche :";
+         // 
+         // BTN_Parcourir
+         // 
+         this.BTN_Parcourir.Location = new System.Drawing.Point(76, 85);
+         this.BTN_Parcourir.Name = "BTN_Parcourir";
+         this.BTN_Parcourir.Size = new System.Drawing.Size(121, 23);
+         this.BTN_Parcourir.TabIndex = 7;
+         this.BTN_Parcourir.Text = "Parcourir";
+         this.BTN_Parcourir.UseVisualStyleBackColor = true;
+         this.BTN_Parcourir.Click += new System.EventHandler(this.BTN_Parcourir_Click);
+         // 
+         // BTN_OK
+         // 
+         this.BTN_OK.Location = new System.Drawing.Point(117, 119);
+         this.BTN_OK.Name = "BTN_OK";
+         this.BTN_OK.Size = new System.Drawing.Size(80, 23);
+         this.BTN_OK.TabIndex = 8;
+         this.BTN_OK.Text = "OK";
+         this.BTN_OK.UseVisualStyleBackColor = true;
+         this.BTN_OK.Click += new System.EventHandler(this.BTN_OK_Click);
+         // 
+         // BTN_Cancel
+         // 
+         this.BTN_Cancel.Location = new System.Drawing.Point(31, 119);
+         this.BTN_Cancel.Name = "BTN_Cancel";
+         this.BTN_Cancel.Size = new System.Drawing.Size(80, 23);
+         this.BTN_Cancel.TabIndex = 9;
+         this.BTN_Cancel.Text = "Annuler";
+         this.BTN_Cancel.UseVisualStyleBackColor = true;
+         this.BTN_Cancel.Click += new System.EventHandler(this.BTN_Cancel_Click);
+         // 
+         // PB_Poster
+         // 
+         this.PB_Poster.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+         this.PB_Poster.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+         this.PB_Poster.Location = new System.Drawing.Point(204, 6);
+         this.PB_Poster.Name = "PB_Poster";
+         this.PB_Poster.Size = new System.Drawing.Size(97, 136);
+         this.PB_Poster.TabIndex = 10;
+         this.PB_Poster.TabStop = false;
+         // 
+         // AjouterModifierSpectacle
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(309, 154);
+         this.Controls.Add(this.PB_Poster);
+         this.Controls.Add(this.BTN_Cancel);
+         this.Controls.Add(this.BTN_OK);
+         this.Controls.Add(this.BTN_Parcourir);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.TB_Artiste);
+         this.Controls.Add(this.TB_Titre);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.CB_Categorie);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.Name = "AjouterModifierSpectacle";
+         this.Text = "AjouterModifierSpectacle";
+         this.Load += new System.EventHandler(this.AjouterModifierSpectacle_Load);
+         ((System.ComponentModel.ISupportInitialize)(this.PB_Poster)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
 
         }
 
@@ -174,6 +178,6 @@
         private System.Windows.Forms.Button BTN_Parcourir;
         private System.Windows.Forms.Button BTN_OK;
         private System.Windows.Forms.Button BTN_Cancel;
-        private System.Windows.Forms.Panel PN_Poster;
+        private System.Windows.Forms.PictureBox PB_Poster;
     }
 }
--- GestionBilletterie/GestionBilletterie/AjouterModifierSpectacle.cs
+++ GestionBilletterie/GestionBilletterie/AjouterModifierSpectacle.cs
@@ -4,127 +4,168 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GestionBilletterie
 {
-    public partial class AjouterModifierSpectacle : Form
-    {
-        bool nouveau;
-        OracleConnection conn = null;
-        string numSpectacle = null;
-        string categorieNum = null;
-        public AjouterModifierSpectacle(OracleConnection connection)
-        {
-            conn = connection;
-            nouveau = true;
-            InitializeComponent();
-        }
-        public AjouterModifierSpectacle(OracleConnection connection, string ID)
-        {
-            conn = connection;
-            numSpectacle = ID;
-            nouveau = false;
-            InitializeComponent();
-        }
+   public partial class AjouterModifierSpectacle : Form
+   {
+      bool nouveau;
+      OracleConnection conn = null;
+      string numSpectacle = null;
+      string categorieNum = null;
+      string ClientId = "68674e2e8d54452";
+      string PosterURL;
 
-        private void AjouterModifierSpectacle_Load(object sender, EventArgs e)
-        {
-            LoadCategorie();
-            if (!nouveau)//modification
+      public AjouterModifierSpectacle(OracleConnection connection)
+      {
+         conn = connection;
+         nouveau = true;
+         InitializeComponent();
+      }
+      public AjouterModifierSpectacle(OracleConnection connection, string ID)
+      {
+         conn = connection;
+         numSpectacle = ID;
+         nouveau = false;
+         InitializeComponent();
+      }
+
+      private void AjouterModifierSpectacle_Load(object sender, EventArgs e)
+      {
+         LoadCategorie();
+         if (!nouveau)//modification
+         {
+            LoadInfo();
+
+         }
+      }
+
+      private void LoadCategorie()
+      {
+         try
+         {
+            OracleCommand inserer = new OracleCommand("INSERTION", conn);
+            inserer.CommandText = "PKG_BILLETS.AFFICHER_CATEGORIE";
+            inserer.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter pResultat = new OracleParameter("RESULTSET", OracleDbType.RefCursor);
+            pResultat.Direction = ParameterDirection.ReturnValue;
+            inserer.Parameters.Add(pResultat);
+
+            OracleDataReader reader = inserer.ExecuteReader();
+
+            while (reader.Read())
             {
-                LoadInfo();
-
+               CB_Categorie.Items.Add(reader.GetString(1));//.Items.Add(reader.GetString(0));
             }
-        }
-
-        private void LoadCategorie()
-        {
-            try
-            {
-                OracleCommand inserer = new OracleCommand("INSERTION", conn);
-                inserer.CommandText = "PKG_BILLETS.AFFICHER_CATEGORIE";
-                inserer.CommandType = CommandType.StoredProcedure;
-
-                OracleParameter pResultat = new OracleParameter("RESULTSET", OracleDbType.RefCursor);
-                pResultat.Direction = ParameterDirection.ReturnValue;
-                inserer.Parameters.Add(pResultat);
-
-                OracleDataReader reader = inserer.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    CB_Categorie.Items.Add(reader.GetString(1));//.Items.Add(reader.GetString(0));
-                }
-            }
-            catch (Exception )
-            {
-                MessageBox.Show("Une érreur est survenue lors de l'initialisation des catégories");
-                this.Close();
-            }
-        }
-
-        private void LoadInfo()
-        {
-            try
-            {
-                // //déclaration de OracleCommand pour appeler la fonction avec la
-                //connection conn.
-                OracleCommand Oracmd = new OracleCommand("PKG_BILLETS",
-                conn);
-                Oracmd.CommandText = "PKG_BILLETS.GET_MODIF_SPECTACLE";
-                Oracmd.CommandType = CommandType.StoredProcedure;
-                // pour une fonction, le paramètre de retour doit être déclaré en
-                //premier.
-                OracleParameter OrapameResultat = new
-                OracleParameter("RESULTSET", OracleDbType.RefCursor);
-                OrapameResultat.Direction = ParameterDirection.ReturnValue;
-                Oracmd.Parameters.Add(OrapameResultat);
-
-                //Déclaration des paramettres
-                OracleParameter procID = new OracleParameter("pNumSpectacle", OracleDbType.Int32);
-                procID.Direction = ParameterDirection.Input;
-                procID.Value = numSpectacle;
-                Oracmd.Parameters.Add(procID);
-
-                // Pour remplir le DataSet, on déclare un OracleDataAdapter pour lequel
-                // on passe notre OracleCommand qui contient TOUS les paramètres.
-                OracleDataReader reader = Oracmd.ExecuteReader();
-                String Categorie = null;
-                while (reader.Read())
-                {
-                    categorieNum = reader.GetInt32(0).ToString();
-                    Categorie = reader.GetString(1);
-                    TB_Titre.Text = reader.GetString(2);
-                    TB_Artiste.Text = reader.GetString(3);
-                } 
-                int i = 0;
-                    foreach (String categorie in CB_Categorie.Items)
-                    {
-                        if (categorie == Categorie)
-                            CB_Categorie.SelectedIndex = i;
-                        else
-                            i++;
-                    }
-            }
-            catch (Exception se)
-            {
-                MessageBox.Show(se.Message.ToString());
-            }
-        }
-
-        private void BTN_Cancel_Click(object sender, EventArgs e)
-        {
+         }
+         catch (Exception)
+         {
+            MessageBox.Show("Une érreur est survenue lors de l'initialisation des catégories");
             this.Close();
-        }
+         }
+      }
 
-        private void BTN_OK_Click(object sender, EventArgs e)
-        {
-            //sauvegarde tout
-            this.Close();
-        }
-    }
+      private void LoadInfo()
+      {
+         try
+         {
+            // //déclaration de OracleCommand pour appeler la fonction avec la
+            //connection conn.
+            OracleCommand Oracmd = new OracleCommand("PKG_BILLETS",
+            conn);
+            Oracmd.CommandText = "PKG_BILLETS.GET_MODIF_SPECTACLE";
+            Oracmd.CommandType = CommandType.StoredProcedure;
+            // pour une fonction, le paramètre de retour doit être déclaré en
+            //premier.
+            OracleParameter OrapameResultat = new
+            OracleParameter("RESULTSET", OracleDbType.RefCursor);
+            OrapameResultat.Direction = ParameterDirection.ReturnValue;
+            Oracmd.Parameters.Add(OrapameResultat);
+
+            //Déclaration des paramettres
+            OracleParameter procID = new OracleParameter("pNumSpectacle", OracleDbType.Int32);
+            procID.Direction = ParameterDirection.Input;
+            procID.Value = numSpectacle;
+            Oracmd.Parameters.Add(procID);
+
+            // Pour remplir le DataSet, on déclare un OracleDataAdapter pour lequel
+            // on passe notre OracleCommand qui contient TOUS les paramètres.
+            OracleDataReader reader = Oracmd.ExecuteReader();
+            String Categorie = null;
+
+            reader.Read();
+            categorieNum = reader.GetInt32(0).ToString();
+            Categorie = reader.GetString(1);
+            TB_Titre.Text = reader.GetString(2);
+            TB_Artiste.Text = reader.GetString(3);
+            //Remplir le picture box
+            System.IO.Stream stream = WebRequest.Create(reader.GetString(4)).GetResponse().GetResponseStream();
+            PB_Poster.BackgroundImage = Bitmap.FromStream(stream);
+
+
+            int i = 0;
+            foreach (String categorie in CB_Categorie.Items)
+            {
+               if (categorie == Categorie)
+                  CB_Categorie.SelectedIndex = i;
+               else
+                  i++;
+            }
+         }
+         catch (Exception se)
+         {
+            MessageBox.Show(se.Message.ToString());
+         }
+      }
+
+      private void BTN_Cancel_Click(object sender, EventArgs e)
+      {
+         this.Close();
+      }
+
+      private void BTN_OK_Click(object sender, EventArgs e)
+      {
+         //sauvegarde tout
+         this.Close();
+      }
+
+      private void BTN_Parcourir_Click(object sender, EventArgs e)
+      {
+         OpenFileDialog dlg = new OpenFileDialog();
+         if (dlg.ShowDialog() == DialogResult.OK)
+         {
+            PosterURL = UploadImage(dlg.FileName);
+            PB_Poster.BackgroundImage = Image.FromFile(dlg.FileName);
+         }
+      }
+
+
+      public string UploadImage(string image) 
+      { 
+         WebClient w = new WebClient();
+         w.Headers.Add("Authorization", "Client-ID " + ClientId);
+         System.Collections.Specialized.NameValueCollection Keys = new System.Collections.Specialized.NameValueCollection();
+         try 
+         {
+            Keys.Add("image", Convert.ToBase64String(File.ReadAllBytes(image)));
+            byte[] responseArray = w.UploadValues("https://api.imgur.com/3/image", Keys);
+            dynamic result = Encoding.ASCII.GetString(responseArray);
+            System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex("link\":\"(.*?)\"");
+            Match match = reg.Match(result);
+            string url = match.ToString().Replace("link\":\"", "").Replace("\"", "").Replace("\\/", "/");
+            return url;
+         } 
+         catch (Exception s) { MessageBox.Show("Something went wrong. " + s.Message); return "Failed!"; 
+         } 
+      }
+
+   }
 }
